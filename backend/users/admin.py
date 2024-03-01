41c2681cfa8cc79ea3f6c604a5d07c946cae0da1
David Pilosyan <pilosyan.david@yandex.ru>
2024-03-01 12:27:11 +0300

    first review:correct admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import TokenProxy
 
-from users.models import User, Subscribe
+from users.models import Subscribe, User
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
 
     list_display = ('email', 'username')
     list_filter = ('email', 'username')
@@ -11,3 +14,5 @@ class UserAdmin(admin.ModelAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Subscribe)
+admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
